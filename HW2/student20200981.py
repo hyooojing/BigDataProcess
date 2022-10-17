febe9d1c6b2c40211bec58282811cc2677215869
hyooojing <wingsbegin@naver.com>
2022-10-18 00:21:24 +0900

    test

--- HW2/student20200981.py
+++ HW2/student20200981.py
@@ -22,6 +22,13 @@ for row in ws:
 list2 = sorted(list, key=lambda x:x[1], reverse=True)
 student = len(list)
 
+a = int(student * 0.3)
+aPlus = int(a * 0.5)
+b = int(student * 0.7) - a
+bPlus = int(b * 0.5)
+c = student - a - b
+cPlus = int(c * 0.5)
+
 for i in range(a):
    ws.cell(row = list2[i][0], column = 8).value = "A0"
 for i in range(aPlus):
@@ -34,33 +41,3 @@ for i in range(a + b, a + b + c):
    ws.cell(row = list2[i][0], column = 8).value = "C0"
 for i in range(a + b, a + b + cPlus):
    ws.cell(row = list2[i][0], column = 8).value = "C+"
-
-wb.save("student.xlsx")
-	if row_id != 1:
-		sum_v = ws.cell(row = row_id, column = 3).value * 0.3
-		sum_v += ws.cell(row = row_id, column = 4).value * 0.35
-		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
-		sum_v += ws.cell(row = row_id, column = 6).value
-		ws.cell(row = row_id, column = 7).value = sum_v
-		total = ws.cell(row = row_id, column = 7).value
-		list.append([row_id, total])
-	row_id += 1
-	
-list2 = sorted(list, key=lambda x:x[1], reverse=True)
-student = len(list)
-
-n = 0
-for i in range(student):
-	ws.cell(row = list2[i][0], column = 8).value = "C0"
-for i in range(int(student * 0.85)):
-	ws.cell(row = list2[i][0], column = 8).value = "C+"
-for i in range(int(student * 0.7)):
-	ws.cell(row = list2[i][0], column = 8).value = "B0"
-for i in range(int(student * 0.5)):
-	ws.cell(row = list2[i][0], column = 8).value = "B+"
-for i in range(int(student * 0.3)):
-	ws.cell(row = list2[i][0], column = 8).value = "A0"
-for i in range(int(student * 0.15)):
-	ws.cell(row = list2[i][0], column = 8).value = "A+"
-
-wb.save("student.xlsx")
