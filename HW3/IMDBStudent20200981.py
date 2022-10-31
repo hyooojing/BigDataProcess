a151e85a15dc1df4187ce2e79c490c8dbd6de057
hyooojing <wingsbegin@naver.com>
2022-10-31 23:22:07 +0900

    IMDBS

--- HW3/IMDBStudent20200981.py
+++ HW3/IMDBStudent20200981.py
@@ -1,1 +1,26 @@
-print("Hello")
+import sys
+
+rFile = sys.argv[1]
+wFile = sys.argv[2]
+gList = []
+dict = {}
+
+f = open(rFile, "rt")
+w = open(wFile, "wt")
+for line in f:
+	movie = line.split('::')
+	genres = movie[2].split('\n')
+	genre = genres[0].split('|')
+	for g in genre:
+		gList.append(g)
+for i in gList:
+	if dict.get(i): 
+		dict[i] += 1
+	else:
+		dict[i] = 1
+for key, value in dict.items():
+	w.write(key + " ")
+	w.write(str(value) + "\n")
+w.close()
+f.close()
+	
